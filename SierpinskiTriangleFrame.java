248f6448246686247a9722c223ef86360f25fc43
YaninaBankova <43926387+YaninaBankova@users.noreply.github.com>
2021-02-14 18:19:46 +0200

    Zadachi 3

---
+++ SierpinskiTriangleFrame.java
@@ -0,0 +1,50 @@
+package edu.smg;
+
+import java.awt.BorderLayout;
+//import java.awt.Graphics;
+//import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class SierpinskiTriangleFrame extends JFrame {
+	//static int r = 0, gr = 0, b = 255;
+
+	private JButton btnMinus = new JButton("-");
+	private JButton btnPlus = new JButton("+");
+	
+	private SierpinskiTrianglePanel trianglePanel = new SierpinskiTrianglePanel(); // To display the pattern
+
+	public SierpinskiTriangleFrame() {
+		// Panel to hold label, text field, and a button
+		JPanel panel = new JPanel();
+		panel.add(btnMinus);
+		panel.add(btnPlus);
+		// Add a Sierpinski triangle panel to the frame
+		add(trianglePanel);
+		add(panel, BorderLayout.SOUTH);
+		// Register a listener
+		btnMinus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(trianglePanel.getOrder() > 0) {
+					trianglePanel.setOrder(trianglePanel.getOrder() - 1);
+					SierpinskiTrianglePanel.gr = 255;
+					SierpinskiTrianglePanel.blue = 0;
+				}
+			}
+		});
+		
+		btnPlus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				trianglePanel.setOrder(trianglePanel.getOrder() + 1);
+				SierpinskiTrianglePanel.gr = 255;
+				SierpinskiTrianglePanel.blue = 0;
+			}
+		});
+		
+
+	}
+
+}
